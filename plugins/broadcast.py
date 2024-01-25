8218805b90e551a042bcfccc340a2b943df101a8
Khurban05 <119966317+Khurban05@users.noreply.github.com>
2024-01-25 13:05:56 +0500

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,8 +4,8 @@ import asyncio
 from pyrogram import Client ,filters
 from helper.database import getid ,delete
 import time
-ADMIN = int(os.environ.get("ADMIN", 923943045))
- 
+ADMIN = int(os.environ.get("ADMIN", 795726700))
+
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
@@ -18,14 +18,14 @@ async def broadcast(bot, message):
    await ms.edit(f"Starting Broadcast .... \n Sending Message To {tot} Users")
    for id in ids:
      try:
-     	time.sleep(1)
-     	await message.reply_to_message.copy(id)
-     	success += 1 
+             time.sleep(1)
+             await message.reply_to_message.copy(id)
+             success += 1 
      except:
-     	failed += 1
-     	delete({"_id":id})     	 
-     	pass
+             failed += 1
+             delete({"_id":id})              
+             pass
      try:
-     	await ms.edit( f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
+             await ms.edit( f"Message sent to {success} chat(s). {failed} chat(s) failed on receiving message. \nTotal - {tot}" )
      except FloodWait as e:
-     	await asyncio.sleep(t.x)
+             await asyncio.sleep(t.x)
